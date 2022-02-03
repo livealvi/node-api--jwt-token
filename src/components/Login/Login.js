924ea7c8e012ef8c47242b1820e1d09c05817a90
Alvi Hasan <livealvi@gmail.com>
2022-02-03 23:11:17 +0600

    :tada: Initial commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,52 @@
+import React, { useContext } from "react";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { UserContext } from "../../App";
+import { useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+const Login = () => {
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+  const history = useHistory();
+  const location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+
+  const app = initializeApp(firebaseConfig);
+  //Google Sign in
+  const googleProvider = new GoogleAuthProvider();
+  const handelGoogleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        const { displayName, email } = user;
+        const signedInUser = { name: displayName, email };
+        setLoggedInUser(signedInUser);
+        console.log(signedInUser);
+        history.replace(from);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log("errorCode:", errorCode);
+        console.log("errorMessage:", errorMessage);
+        console.log("email:", email);
+        console.log("credential:", credential);
+      });
+  };
+  return (
+    <div>
+      <h1>This is Login</h1>
+      <button onClick={handelGoogleSignIn}>Google Sign in</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAva6RnRQWxPFwaOaWsC5mQyxT4czXXo_0",
+  authDomain: "burj-c0be9.firebaseapp.com",
+  projectId: "burj-c0be9",
+  storageBucket: "burj-c0be9.appspot.com",
+  messagingSenderId: "634842958340",
+  appId: "1:634842958340:web:975c79198b5775ea434351",
+};
+
+export default firebaseConfig;
